e5edbfaf63e65af9b1e8b7ba92d0beb02df9b6d2
Miktlan <2mik.samuel@gmail.com>
2026-02-11 14:34:02 +0100

    python_dz_10

--- Samoilenko_dz_python_10.py
+++ Samoilenko_dz_python_10.py
@@ -13,15 +13,11 @@
 """
 
 cost = int(input("Введите стоимость товара: "))
-funfzig = cost//50
-zehn = (cost-(50*funfzig))//10
-funf = (cost-(10*zehn+50*funfzig))//5
-zwei = (cost-(5*funf+10*zehn+50*funfzig))//2
-print(f"Внесите монеты номиналом 50: {funfzig}",
-      f"Внесите монеты номиналом 10: {zehn}",
-      f"Внесите монеты номиналом 5: {funf}",
-      f"Внесите монеты номиналом 2: {zwei}",
-      sep="\n")
+nom = [50, 10, 5, 2]
+for nom in nom:
+    coin = cost // nom
+    cost -= nom * coin
+    print(f"Внесите монеты номиналом {nom}: {coin}")
 
 """ 
 Задание №2
@@ -33,11 +29,11 @@ numbers = [4, 9, 1, 7, 2, 5, 0, 3, 7, 1, 3]
 Измененный список:  [4, 81, 1, 49, 2, 25, 0, 9, 49, 1, 9]
 """
 
-numbers = [4, 9, 1, 7, 2, 5, 0, 3, 7, 1, 3]
-print("Изначальный список:", numbers)
-for i in range(len(numbers)):
-    if numbers[i] % 2 == 0:
-        continue
-    else:
-        numbers[i] = numbers[i] ** 2
-print("Измененный список: ", numbers)
+# numbers = [4, 9, 1, 7, 2, 5, 0, 3, 7, 1, 3]
+# print("Изначальный список:", numbers)
+# for i in range(len(numbers)):
+#     if numbers[i] % 2 == 0:
+#         continue
+#     else:
+#         numbers[i] = numbers[i] ** 2
+# print("Измененный список: ", numbers)
