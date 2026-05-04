e74a5ee6e039593eb98a318dd67b43efaa2c69e2
Mikt1an <2mik.samuel@gmail.com>
2026-05-04 13:37:59 +0200

    Update Samoilenko_dz_python_18.py

--- Samoilenko_dz_python_18.py
+++ Samoilenko_dz_python_18.py
@@ -1,29 +1,9 @@
-# Задача №1
-# Не уникальные числа
-# Напишите программу, которая находит все числа, встречающиеся более одного раза в списке,
-# и выводит их в порядке убывания.
-# Данные:
-# numbers = [4, 7, 3, 7, 8, 3, 4, 2, 7, 3, 8, 4]
-# Пример вывода:
-# Числа, встречающиеся более одного раза: [7, 4, 3, 8]
-
 
 numbers = [4, 7, 3, 7, 8, 3, 4, 2, 7, 3, 8, 4]
 new_numbers = {numb for numb in numbers if numbers.count(numb) > 1}
-print("Числа, встречающиеся более одного раза: ", sorted(new_numbers, reverse=True))
-
-# Задача №2
-# Проверка подмножества Задача:
-# Напишите программу, которая проверяет, является ли один словарь подмножеством другого
-# (т.е. все его пары "ключ-значение" содержатся в другом словаре).
-# Данные:
-# dict1 = {"a": 1, "b": 2}
-# dict2 = {"a": 1, "b": 2, "c": 3}
-# Пример вывода:
-# Первый словарь является подмножеством второго.
+print("Numbers that occur more than once: ", sorted(new_numbers, reverse=True))
 
 dict1 = {"a": 1, "b": 2}
 dict2 = {"a": 1, "b": 2, "c": 3}
-print("Первый словарь является подмножеством второго." if dict1.items() <= dict2.items() else "Первый словарь не является подмножеством второго.")
-print("Второй словарь является подмножеством первого." if dict2.items() <= dict1.items() else "Второй словарь не является подмножеством первого.")
-
+print("The first dictionary is a subset of the second." if dict1.items() <= dict2.items() else "The first dictionary is not a subset of the second.")
+print("The second dictionary is a subset of the first." if dict2.items() <= dict1.items() else "The second dictionary is not a subset of the first.")
