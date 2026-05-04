05c082661d232508652b2edc65d09355ae980eac
Mikt1an <2mik.samuel@gmail.com>
2026-05-04 13:29:17 +0200

    Update Samoilenko_dz_python_15.py

--- Samoilenko_dz_python_15.py
+++ Samoilenko_dz_python_15.py
@@ -1,13 +1,3 @@
-"""
-Задача№1
-Одно слово
-Напишите программу, которая обрабатывает список из строк и удаляет все строки, содержащие более одного слова,
-а также преобразует оставшиеся строки к нижнему регистру.
-Данные:
-text_list = ["Hello", "Python Programming", "World", "Advanced Topics", "Simple"]
-Пример вывода:
-Обработанный список: ['hello', 'world', 'simple']
-"""
 
 text_list = ["Hello", "Python Programming", "World", "Advanced Topics", "Simple"]
 for symbol in text_list[:]:
@@ -18,24 +8,8 @@ for symbol in text_list[:]:
         text_list[index] = symbol.lower()
 print(text_list)
 
-"""
-Задача№2
-Обновление цен товаров
-Дан список товаров с ценами. Программа должна применить скидку к каждому товару и добавить в список элемент с новой ценой. 
-В конце вывести таблицу с новой ценой.
-Данные:
 products = [["Laptop", 1200], ["Mouse", 25], ["Keyboard", 75], ["Monitor", 200]]
-Пример вывода:
-Введите скидку (в процентах): 17
-Товар          Старая цена    Новая цена
-Laptop            1200.00$       996.00$
-Mouse                25.00$         20.75$
-Keyboard           75.00$         62.25$
-Monitor            200.00$       166.00$
-"""
-
-products = [["Laptop", 1200], ["Mouse", 25], ["Keyboard", 75], ["Monitor", 200]]
-rabat = int(input("Введите скидку (в процентах): ")) / 100
+rabat = int(input("Enter discount (in procent): ")) / 100
 print(f"{"Name":<10}", f"{"Old Price":>11}", f"{"New Price":>15}")
 for product in products:
     new_price = product[1] - (product[1] * rabat)
