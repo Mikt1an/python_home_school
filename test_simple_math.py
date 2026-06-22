a7ebd07f19d1789e4d6555c7284e7a0380fc6a23
Mikt1an <2mik.samuel@gmail.com>
2026-06-22 15:24:25 +0200

    add pytest.fixture

--- test_simple_math.py
+++ test_simple_math.py
@@ -1,25 +1,30 @@
+import pytest
 from simple_math import SimpleMath
 
 
-def test_square_positive():
-    assert SimpleMath().square(2) == 4
+@pytest.fixture
+def math():
+    return SimpleMath()
+
+def test_square_positive(math):
+    assert math.square(2) == 4
 
 
-def test_square_negative():
-    assert SimpleMath().square(-3) == 9
+def test_square_negative(math):
+    assert math.square(-3) == 9
 
 
-def test_square_zero():
-    assert SimpleMath().square(0) == 0
+def test_square_zero(math):
+    assert math.square(0) == 0
 
 
-def test_cube_positive():
-    assert SimpleMath().cube(2) == 8
+def test_cube_positive(math):
+    assert math.cube(2) == 8
 
 
-def test_cube_negative():
-    assert SimpleMath().cube(-3) == -27
+def test_cube_negative(math):
+    assert math.cube(-3) == -27
 
 
-def test_cube_zero():
-    assert SimpleMath().cube(0) == 0
+def test_cube_zero(math):
+    assert math.cube(0) == 0
